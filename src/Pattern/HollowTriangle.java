e79280758f235e375432d4580bf0e7bbcb2a8c17
WebWithRathor <devrajrathor143@gmail.com>
2024-11-18 23:32:10 +0530

    hollow triangle

--- src/Pattern/HollowTriangle.java
+++ src/Pattern/HollowTriangle.java
@@ -9,7 +9,7 @@ public class HollowTriangle {
         for (int i = 1; i <= n; i++) {
             for (int j = 1; j <= n; j++) {
                 if ( i == n || i == j || j==1) {
-                    System.out.print("* ");
+                    System.out.print(" *");
                 } else {
                     System.out.print("  ");
                 }
