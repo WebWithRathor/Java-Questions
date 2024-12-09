a6587bbb27a8864c3098a7c21694d06fdd05d1c9
WebWithRathor <devrajrathor143@gmail.com>
2024-12-09 22:53:26 +0530

    ArrayList

--- src/ArrayList/Intro.java
+++ src/ArrayList/Intro.java
@@ -9,8 +9,11 @@ public class Intro {
         arr.add(2);
         arr.add(3);
         arr.add(4);
+        arr.add(4,50);
         System.out.println(arr);
-        arr.remove(Integer.valueOf(1));
+//        arr.remove(Integer.valueOf(1));
+//        arr.remove((Integer)(50));
+        arr.set(4,100);
         System.out.println(arr);
         arr.remove(1);
         System.out.println(arr);
